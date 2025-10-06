b112a4e85db8011f77225f0b4b92c0c9387a05b8
andreeeestor <andrenestor0503@gmail.com>
2025-10-06 00:35:33 -0300

    feat: NeonDB implementation + API Routes

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,6 +2,7 @@ import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
 import { images } from "@/constants";
+import { fetchAPI } from "@/lib/fetch";
 import { useSignUp } from "@clerk/clerk-expo";
 import { Link, router } from "expo-router";
 import { useState } from "react";
@@ -53,7 +54,15 @@ export default function SignUpPage() {
       });
 
       if (signUpAttempt.status === "complete") {
-        // dps criar o usuário no banco de dados
+        await fetchAPI("/(api)/user", {
+          method: "POST",
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: signUpAttempt.createdUserId,
+          }),
+        });
+
         await setActive({ session: signUpAttempt.createdSessionId });
         setVerification({ ...verification, state: "success" });
       } else {
